b6c7e5e1956fb4e38cc11de8b584118b52f04727
Matvey <morozovmatvej75@gmail.com>
2022-11-02 12:08:57 +0300

    Installed react-split

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { nanoid } from "nanoid"
+import Split from "react-split"
 
-export default function App() {
-  const [idElems, setIdElems] = useState([])
-  useEffect(() => {
-    for (let i = 0; i < 50; i++) {
-      let id = nanoid(30)
-      setIdElems(prevState => [...prevState, <p key={id}>{id}</p>])
-    }
-  }, [])
-    
-  console.log(idElems)
-  
+export default function App() {  
   return (
     <main>
-      <h1>Test</h1>
-      {idElems}
+      <h1>oisdjflk</h1>
+      <Split />
     </main>
   );
 }
