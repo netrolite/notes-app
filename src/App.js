e5116f70767d140cdd953bab1080bfd5c5e212c1
Matvey <morozovmatvej75@gmail.com>
2022-11-02 15:47:02 +0300

    minor update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { nanoid } from "nanoid"
-import Split from "react-split"
 
 export default function App() {  
   return (
     <main>
       <h1>oisdjflk</h1>
-      <Split />
+      
     </main>
   );
 }
