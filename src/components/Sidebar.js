565cd28c369b285335e82a37c72e551b390b071a
Matvey <morozovmatvej75@gmail.com>
2022-11-03 15:59:24 +0300

    Added sidebar & editor & basic styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 
 export default function Sidebar() {
     return (
-        <div>sidebar</div>
+        <aside className="sidebar">
+            <div className="sidebar-header">
+                <h1 className="sidebar-title">Notes</h1>   
+                <div className="add-note">+</div>
+            </div>
+        </aside>
     )
 }
