edc4077cd33d9f3223a2494764ea8f6cdb416441
Matvey <morozovmatvej75@gmail.com>
2022-11-03 16:15:34 +0300

    Added "add new note" button & more styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,9 +3,17 @@ import React, { useState, useEffect } from "react";
 export default function Sidebar() {
     return (
         <aside className="sidebar">
-            <div className="sidebar-header">
-                <h1 className="sidebar-title">Notes</h1>   
-                <div className="add-note">+</div>
+            <div className="sidebar-header">   
+                <button className="add-note">Add new note</button>
+            </div>
+            <div className="sidebar-notes-list">
+                <div className="note"></div>
+                <div className="note"></div>
+                <div className="note"></div>
+                <div className="note"></div>
+                <div className="note"></div>
+                <div className="note"></div>
+                <div className="note"></div>
             </div>
         </aside>
     )
