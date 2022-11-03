e6e40ebf3a2a0b2e6ccc054d519e66f945995ba5
Matvey <morozovmatvej75@gmail.com>
2022-11-03 15:23:47 +0300

    Created needed components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { nanoid } from "nanoid"
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
 
 export default function App() {  
   return (
     <main className="main">
-      
+      <Sidebar />
+      <Editor />
     </main>
   );
 }
