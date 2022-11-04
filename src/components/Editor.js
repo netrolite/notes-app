e12eddf8d058fb5462b304f08a8cf61c7295be79
Matvey <morozovmatvej75@gmail.com>
2022-11-04 19:12:54 +0300

    Added dark mode switch

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -4,8 +4,11 @@ export default function Editor() {
     return (
         <div className="editor">
             <div className="top-bar">
-                <div className="dark-mode-switch">
-                    <div className="dark-mode-switch-circle"></div>
+                <div className="dark-mode">
+                    <div className="dark-mode-switch">
+                        <div className="dark-mode-switch-circle"></div>
+                    </div>
+                    <div className="dark-mode-label">Dark Mode</div>
                 </div>
             </div>
         </div>
