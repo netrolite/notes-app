c11118edf4ec058f6ce6b480e9cfc6726794cae5
Matvey <morozovmatvej75@gmail.com>
2022-11-04 14:47:04 +0300

    Sidebar scrollbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -63,6 +63,150 @@ export default function Sidebar() {
                         </div>
                     </div>
                 </div>
+
+                <div className="note">
+                    <div className="note-inside">
+                        <div className="note-title">
+                            Title
+                        </div>
+                        <div className="note-subtitle">
+                            <div className="note-date">12/10/2022 </div>
+                            <div className="note-snippet">lorem</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="note above-selected">
+                    <div className="note-inside">
+                        <div className="note-title">
+                            Title
+                        </div>
+                        <div className="note-subtitle">
+                            <div className="note-date">12/10/2022 </div>
+                            <div className="note-snippet">lorem</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="note selected">
+                    <div className="note-inside">
+                        <div className="note-title">
+                            Titlesdfasdfasdfsdfsdfa k k k k k 
+                        </div>
+                        <div className="note-subtitle">
+                            <div className="note-date">12/10/2022</div>
+                            <div className="note-snippet">loremsdfsdf dfdf kkkk</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="note">
+                    <div className="note-inside">
+                        <div className="note-title">
+                            Title
+                        </div>
+                        <div className="note-subtitle">
+                            <div className="note-date">12/10/2022 </div>
+                            <div className="note-snippet">lorem</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="note">
+                    <div className="note-inside">
+                        <div className="note-title">
+                            Title
+                        </div>
+                        <div className="note-subtitle">
+                            <div className="note-date">12/10/2022 </div>
+                            <div className="note-snippet">lorem</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="note above-selected">
+                    <div className="note-inside">
+                        <div className="note-title">
+                            Title
+                        </div>
+                        <div className="note-subtitle">
+                            <div className="note-date">12/10/2022 </div>
+                            <div className="note-snippet">lorem</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="note selected">
+                    <div className="note-inside">
+                        <div className="note-title">
+                            Titlesdfasdfasdfsdfsdfa k k k k k 
+                        </div>
+                        <div className="note-subtitle">
+                            <div className="note-date">12/10/2022</div>
+                            <div className="note-snippet">loremsdfsdf dfdf kkkk</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="note">
+                    <div className="note-inside">
+                        <div className="note-title">
+                            Title
+                        </div>
+                        <div className="note-subtitle">
+                            <div className="note-date">12/10/2022 </div>
+                            <div className="note-snippet">lorem</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="note">
+                    <div className="note-inside">
+                        <div className="note-title">
+                            Title
+                        </div>
+                        <div className="note-subtitle">
+                            <div className="note-date">12/10/2022 </div>
+                            <div className="note-snippet">lorem</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="note above-selected">
+                    <div className="note-inside">
+                        <div className="note-title">
+                            Title
+                        </div>
+                        <div className="note-subtitle">
+                            <div className="note-date">12/10/2022 </div>
+                            <div className="note-snippet">lorem</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="note selected">
+                    <div className="note-inside">
+                        <div className="note-title">
+                            Titlesdfasdfasdfsdfsdfa k k k k k 
+                        </div>
+                        <div className="note-subtitle">
+                            <div className="note-date">12/10/2022</div>
+                            <div className="note-snippet">loremsdfsdf dfdf kkkk</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="note">
+                    <div className="note-inside">
+                        <div className="note-title">
+                            Title
+                        </div>
+                        <div className="note-subtitle">
+                            <div className="note-date">12/10/2022 </div>
+                            <div className="note-snippet">lorem</div>
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         </aside>
