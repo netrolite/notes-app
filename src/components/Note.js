6fc8dcbe32ae577ead556aa37ae1fcd6b5494f1c
Matvey <morozovmatvej75@gmail.com>
2022-11-08 15:53:09 +0300

    Made Note component

---
+++ src/components/Note.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Note(props) {
+    <div
+        className={
+            "note" += props.isSelected ? " selected" : "" 
+            += props.isAboveSelected ? " above-selected" : ""
+        }
+        onClick={ev => {props.selectNote(ev, props.id)}}
+        key={props.key}
+    >
+    <div className="note-inside">
+        <div className="note-title">
+            {props.title}
+        </div>
+        <div className="note-subtitle">
+            <div className="note-date">{props.date}</div>
+            <div className="note-snippet">
+                {props.textSnippet ? props.textSnippet : "No Additional Text"}
+            </div>
+        </div>
+    </div>
+</div>
+}
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,49 +12,7 @@ export default function Sidebar() {
        (notes[0] && notes[0].id) || ""
     )
 
-
-    function createNote() {
-        // getting all previously created notes OR an empty string
-        const prevNotes = JSON.parse(localStorage.getItem("notes")) || "";
-
-        const newNote = {
-            id: nanoid(12),
-            title: "New Note",
-            textSnippet: "No additional text",
-            text: "",
-            date: new Date().getSeconds(),
-        };
         
-    }
-
-    function selectNote(ev, id) {
-        // !!! compare current note id to id passed as an argument
-        console.log("selected");
-    }
-
-    const notesElements = notes.map((item, index) => {
-        return (
-            <div
-                className={
-                    `note${item.isSelected ? " selected" : ""}`
-                }
-                onClick={ev => {selectNote(ev, item.id)}}
-                key={index}
-            >
-                <div className="note-inside">
-                    <div className="note-title">
-                        {item.title}
-                    </div>
-                    <div className="note-subtitle">
-                        <div className="note-date">{item.date}</div>
-                        <div className="note-snippet">
-                            {item.textSnippet ? item.textSnippet : "No Additional Text"}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        ) 
-    });
     
     return (
         <aside className="sidebar">
