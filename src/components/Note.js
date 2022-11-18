11063528abbbe5ab8c5b1d31f306b85646b28d90
Matvey <morozovmatvej75@gmail.com>
2022-11-18 17:20:27 +0300

    Revert "Revert "Revert "rich text editor"""
    This reverts commit 987fc1b0599ba13bcf6886784a08bb66770d6de4.

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 
 export default function Note(props) {
     // first line of "props.text"
-    console.log(props.text);
     const title = props.text.split(/\n/)[0] || "New Note";
     // second line of "props.text"
     const textSnippet = props.text.split(/\n/)[1] || "No Additional Text";
