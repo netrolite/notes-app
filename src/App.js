bd79a3bdaca56c60c2e303e3a403bc2a030974ec
Matvey <morozovmatvej75@gmail.com>
2022-11-18 17:21:32 +0300

    Revert "rich text editor"
    This reverts commit 008580ba211b8b3be15803fcab150dc22b0d5abd.

--- src/App.js
+++ src/App.js
@@ -38,7 +38,6 @@ export default function App() {
       }
     }
 
-    // update prevWindowWidth
     prevWindowWidth = window.innerWidth;
   })
 
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -9,8 +9,7 @@ export default function Editor(props) {
     )
 
     // runs when user updates text inside "editor"
-    function updateNote(e) {
-        console.log(e.target.innerText);
+    function updateNote() {
         // if there are no notes
         if(props.notes.length === 0) {
             const newID = nanoid();
@@ -18,7 +17,7 @@ export default function Editor(props) {
             // add new note to "notes" state
             props.setNotes(() => {
                 return [{
-                    text: e.target.innerText,
+                    text: document.querySelector(".editor").value,
                     id: newID,
                     date: new Date().getTime()
                 }]
@@ -32,7 +31,7 @@ export default function Editor(props) {
             const unchangedNotes = props.notes.filter(item => item.id !== props.currNoteID);
             // updated currently selected note
             const updatedNote = props.notes[currNoteIndex];
-            updatedNote.text = e.target.innerText;
+            updatedNote.text = document.querySelector(".editor").value;
             // adding 1 second to make timer restart on 0, rather than 1
             updatedNote.date = new Date().getTime() + 1000;
 
@@ -50,24 +49,22 @@ export default function Editor(props) {
         document.documentElement.scrollTo(0, 0);
     })
 
-    let editorValue = ""
+    let textareaValue = ""
 
     if(props.notes.length > 0) {
         if(currNoteIndex > -1) {
-            editorValue = props.notes[currNoteIndex].text;
+            textareaValue = props.notes[currNoteIndex].text;
         }
     }
     else {
-        editorValue = "";
+        textareaValue = "";
     }
 
     return (
-        <div 
+        <textarea 
             className="editor"
-            onInput={e => updateNote(e)} 
-            contentEditable="true"
-        >
-            {editorValue}
-        </div> 
+            onChange={updateNote} 
+            value={textareaValue}
+        /> 
     )
 }
